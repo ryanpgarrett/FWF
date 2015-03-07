8fbf77e6a176553ea4cd242bc5e921263e7137a8
Ryan Garrett <ryan.p.garrett@gmail.com>
2015-03-06 17:58:44 -0800

    Initial
    initial

---
+++ Week1/js/app.js
@@ -0,0 +1,14 @@
+function ShopController($scope){
+    $scope.itemName;
+    $scope.itemArray = ["one","two","three"];
+    $scope.addItem = function(){
+        $scope.itemArray.push($scope.itemName);
+        $scope.itemName = " ";
+    }
+
+    $scope.deleteItem = function(deletedItem){
+        var idx = $scope.itemArray.indexOf(deletedItem);
+        $scope.itemArray.splice(idx, 1);
+    }
+
+}
