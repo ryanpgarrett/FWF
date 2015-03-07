fffda6e8363756fe85bfe0260db926d67260f1d1
Ryan Garrett <ryan.p.garrett@gmail.com>
2015-03-06 18:24:40 -0800

    2
    2

--- Week1/js/app.js
+++ Week1/js/app.js
@@ -3,7 +3,7 @@ function ShopController($scope){
     $scope.itemArray = ["one","two","three"];
     $scope.addItem = function(){
         $scope.itemArray.push($scope.itemName);
-        $scope.itemName = " ";
+        $scope.itemName = "";
     }
 
     $scope.deleteItem = function(deletedItem){
